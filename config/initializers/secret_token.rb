af61b323ac2d4a5bf215837058982736b8eb49f5
Vu Pham <vupham0326@mail.com>
2015-05-27 12:38:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Warriors::Application.config.secret_key_base = '659a5d59e6a3a836ee9cf49765bd38cbfdf7db604421c9428199c31a09ea74787e51d19964c308632c87e0800a79789920288241c4a99de2c713002c8c35982e'
