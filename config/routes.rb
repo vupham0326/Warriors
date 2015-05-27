2d31a0b89133a6abe21ad64802d160fbe84b2291
Vu Pham <vupham0326@mail.com>
2015-05-27 13:16:21 +0000

    setting up the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Warriors::Application.routes.draw do
+  root 'texts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
